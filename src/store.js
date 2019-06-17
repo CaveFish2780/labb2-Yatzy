8ba1a59e13d25531e6521968277a8b7b963d20b6
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-17 02:36:59 +0200

    added states: count, yatzyTable, dices

--- src/store.js
+++ src/store.js
@@ -5,6 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    
+    count: 0,
+
+    yatzyTable: [],
+
+    dices:[    
+    {name: 'd1', value: 0, selected: false}, 
+    {name: 'd2', value: 0, selected: false},
+    {name: 'd3', value: 0, selected: false}, 
+    {name: 'd4', value: 0, selected: false},
+    {name: 'd5', value: 0, selected: false}
+  ],
 
   },
   mutations: {
