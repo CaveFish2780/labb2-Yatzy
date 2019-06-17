39c9b72540e24d75762ebf6f36d00d9f06646225
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-17 02:47:32 +0200

    added components: Table, Dice

---
+++ src/components/Game.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import Table from './Table.vue'
+import Dice from './Dice.vue'
+
+export default {
+    
+    name: 'game',
+    components: {
+        Table,
+        Dice,
+  }, 
+    methods:{
+
+    },
+    }
+</script>
+
+<style scoped>
+
+</style>
